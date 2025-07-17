ad960a16e6e9bbd58c23934d3b3d898cb5da3f39
Dan Scholem <dan.scholem@gmail.com>
2025-07-17 19:00:55 -0400

    enforce passwords being at least 6 chars

--- src/main/java/com/bloodpressuremonitor/group4/csc325_group4/model/SignUpModel.java
+++ src/main/java/com/bloodpressuremonitor/group4/csc325_group4/model/SignUpModel.java
@@ -14,14 +14,15 @@ public class SignUpModel {
     private User newUser;
 
     public boolean registerUser(String email, String password, String firstName, String lastName, LocalDate dateOfBirth, int age) {
-        UserRecord.CreateRequest request = new  UserRecord.CreateRequest()
-                .setEmail(email)
-                .setEmailVerified(false)
-                .setPassword(password)
-                .setDisabled(false);
 
-        UserRecord userRecord;
         try {
+            UserRecord.CreateRequest request = new  UserRecord.CreateRequest()
+                    .setEmail(email)
+                    .setEmailVerified(false)
+                    .setPassword(password)
+                    .setDisabled(false);
+
+            UserRecord userRecord;
             userRecord = App.fauth.createUser(request);
             newUser = new User(firstName, lastName, email, age, dateOfBirth, userRecord.getUid());
             System.out.println("Successfully registered user: \n"  + newUser.toString());
@@ -40,6 +41,12 @@ public class SignUpModel {
             return false;
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (RuntimeException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(e.getMessage());
+            alert.showAndWait();
+            return false;
         }
 
     }
